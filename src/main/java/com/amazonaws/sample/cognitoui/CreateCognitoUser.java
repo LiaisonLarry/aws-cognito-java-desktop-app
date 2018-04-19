460d6f037d53569b3b070b7631925b97a5b9d077
Larry Tambascio <ltambascio@liaisonedu.com>
2018-04-19 15:45:56 -0400

    Added challenge request after updating user

--- src/main/java/com/amazonaws/sample/cognitoui/CreateCognitoUser.java
+++ src/main/java/com/amazonaws/sample/cognitoui/CreateCognitoUser.java
@@ -1,9 +1,14 @@
 package com.amazonaws.sample.cognitoui;
 
+import static com.amazonaws.services.cognitoidp.model.AuthFlowType.ADMIN_NO_SRP_AUTH;
+import static com.amazonaws.services.cognitoidp.model.ChallengeNameType.NEW_PASSWORD_REQUIRED;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -13,7 +18,17 @@ import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
 import com.amazonaws.services.cognitoidp.model.AdminCreateUserRequest;
 import com.amazonaws.services.cognitoidp.model.AdminCreateUserResult;
+import com.amazonaws.services.cognitoidp.model.AdminInitiateAuthRequest;
+import com.amazonaws.services.cognitoidp.model.AdminInitiateAuthResult;
+import com.amazonaws.services.cognitoidp.model.AdminRespondToAuthChallengeRequest;
+import com.amazonaws.services.cognitoidp.model.AdminRespondToAuthChallengeResult;
+import com.amazonaws.services.cognitoidp.model.AdminUpdateUserAttributesRequest;
+import com.amazonaws.services.cognitoidp.model.AdminUpdateUserAttributesResult;
 import com.amazonaws.services.cognitoidp.model.AttributeType;
+import com.amazonaws.services.cognitoidp.model.AuthFlowType;
+import com.amazonaws.services.cognitoidp.model.ChallengeNameType;
+import com.amazonaws.services.cognitoidp.model.InitiateAuthRequest;
+import com.amazonaws.services.cognitoidp.model.InitiateAuthResult;
 
 public class CreateCognitoUser {
 	
@@ -85,6 +100,54 @@ public class CreateCognitoUser {
 			System.out.println(result);
 		} catch (Exception e) {
 			System.out.println(e);
+			return;
+		}
+		
+		// Now update it
+		AdminUpdateUserAttributesRequest updateRequest = new AdminUpdateUserAttributesRequest();
+		updateRequest.setUsername("sophas-user-5");
+		updateRequest.setUserPoolId(POOL_ID);
+		
+		list = new ArrayList<>();
+		AttributeType custAttribute = new AttributeType();
+		custAttribute.setName("custom:FORM_ID");
+		custAttribute.setValue("6396");
+		list.add(custAttribute);
+		
+		updateRequest.setUserAttributes(list);
+		
+		try {
+			AdminUpdateUserAttributesResult updResult = cognitoIdentityProvider.adminUpdateUserAttributes(updateRequest);
+			System.out.println("Update result: " + updResult);
+		}
+		catch (Exception e) {
+			System.out.println("Exception while updating:" + e);
+		}
+		
+		// Update auth challenge
+		AdminInitiateAuthRequest adminInitiateAuthRequest = new AdminInitiateAuthRequest();
+		adminInitiateAuthRequest.setAuthFlow(ADMIN_NO_SRP_AUTH);
+		adminInitiateAuthRequest.setClientId(CLIENTAPP_ID);
+		adminInitiateAuthRequest.setUserPoolId(POOL_ID);
+		adminInitiateAuthRequest.addAuthParametersEntry("USERNAME", "sophas-user-5");
+		adminInitiateAuthRequest.addAuthParametersEntry("PASSWORD", "password4321");
+		
+		AdminInitiateAuthResult initiateAuthResult = cognitoIdentityProvider.adminInitiateAuth(adminInitiateAuthRequest);
+		System.out.println("challenge name=" + initiateAuthResult.getChallengeName());
+		if (initiateAuthResult.getChallengeName().equals(NEW_PASSWORD_REQUIRED.toString())) {
+			
+			AdminRespondToAuthChallengeRequest authChallengeRequest = new AdminRespondToAuthChallengeRequest();
+			authChallengeRequest.setChallengeName(NEW_PASSWORD_REQUIRED);
+			authChallengeRequest.setUserPoolId(POOL_ID);
+			authChallengeRequest.setClientId(CLIENTAPP_ID);
+			authChallengeRequest.setSession(initiateAuthResult.getSession());
+			Map<String, String> challResponses = new HashMap<>();
+			challResponses.put("USERNAME", "sophas-user-5");
+			challResponses.put("NEW_PASSWORD", "password4321");
+			authChallengeRequest.setChallengeResponses(challResponses);
+			
+			AdminRespondToAuthChallengeResult authChallengeResult = cognitoIdentityProvider.adminRespondToAuthChallenge(authChallengeRequest);
+			System.out.println("Auth challenge response: " + authChallengeResult);
 		}
 	}
 	
